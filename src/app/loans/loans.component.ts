f0de214b944beb6174fadac81d8d9ea52101a77f
willianfalbo <willianfalbo@gmail.com>
2019-09-20 01:20:10 -0300

    Configured application to be built in "loan-app" virtual-direct. It will be used in my custom domain "www.willianfalbo.com/loan-app"

--- src/app/loans/loans.component.ts
+++ src/app/loans/loans.component.ts
@@ -17,37 +17,37 @@ export class LoansComponent implements OnInit {
         id: 'capital-giro',
         name: 'Capital de Giro',
         description: 'Detalhes capital de giro',
-        imageUrl: '../../assets/img/loan/capital-giro.png',
+        imageUrl: 'assets/img/loan/capital-giro.png',
       },
       {
         id: 'emprestimo-pessoal',
         name: 'Emprestimo Pessoal',
         description: 'Detalhes emprestimo pessoal',
-        imageUrl: '../../assets/img/loan/emprestimo-pessoal.png',
+        imageUrl: 'assets/img/loan/emprestimo-pessoal.png',
       },
       {
         id: 'financiamento-casa',
         name: 'Financiamento Casa',
         description: 'Detalhes financiamento casa',
-        imageUrl: '../../assets/img/loan/finaciamento-casa.png',
+        imageUrl: 'assets/img/loan/finaciamento-casa.png',
       },
       {
         id: 'financiamento-divida',
         name: 'Financiamento Dívida',
         description: 'Detalhes financiamento de dívida',
-        imageUrl: '../../assets/img/loan/finaciamento-divida.png',
+        imageUrl: 'assets/img/loan/finaciamento-divida.png',
       },
       {
         id: 'financiamento-carro',
         name: 'Financiamento Carro',
         description: 'Detalhes financiamento de carro',
-        imageUrl: '../../assets/img/loan/financiamento-carro.png',
+        imageUrl: 'assets/img/loan/financiamento-carro.png',
       },
       {
         id: 'investimento-negocio',
         name: 'Investimento Negócio',
         description: 'Detalhes investimento negócio',
-        imageUrl: '../../assets/img/loan/investimento-negocio.png',
+        imageUrl: 'assets/img/loan/investimento-negocio.png',
       },
     ];
   }
